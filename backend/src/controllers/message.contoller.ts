51b50c136a73bbb509ae254221695027fd151cb6
lihasahil <sahilshrestha106@gmail.com>
2025-09-13 12:51:31 +0545

    feature: message  routes

---
+++ backend/src/controllers/message.contoller.ts
@@ -0,0 +1,65 @@
+import { Request, Response } from "express";
+import User from "../models/user.model";
+import Message from "../models/message.model";
+import cloudinary from "../lib/cloudinary";
+
+export const getUsersForSidebar = async (req: Request, res: Response) => {
+  try {
+    const loggedInUserId = req.user?._id;
+    const filteredusers = await User.find({
+      _id: { $ne: loggedInUserId },
+    }).select("-password");
+
+    res.status(200).json(filteredusers);
+  } catch (error) {
+    console.log("Error in getUserForSidebar", error);
+    res.status(500).json({ error: "Internal server Error" });
+  }
+};
+
+export const getMessages = async (req: Request, res: Response) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const myId = req.user?._id;
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: userToChatId },
+        { senderId: userToChatId, receiverId: myId },
+      ],
+    });
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in getMessages", error);
+    res.status(500).json({ error: "Internal server Error" });
+  }
+};
+
+export const sendMessage = async (req: Request, res: Response) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user?._id;
+
+    let imageUrl;
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    await newMessage.save();
+
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.log("Error in sendMessage", error);
+    res.status(500).json({ error: "Internal server Error" });
+  }
+};
---
+++ backend/src/models/message.model.ts
@@ -0,0 +1,36 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IMessage extends Document {
+  senderId: mongoose.Types.ObjectId;
+  receiverId: mongoose.Types.ObjectId;
+  text?: string;
+  image?: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const messageSchema = new Schema<IMessage>(
+  {
+    senderId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiverId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    text: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const Message = mongoose.model<IMessage>("Message", messageSchema);
+
+export default Message;
