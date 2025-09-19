474ef8a8cdeb85c4f66de4eaa9582db873e98e53
lihasahil <sahilshrestha106@gmail.com>
2025-09-19 21:31:07 +0545

    feature: login and signup integration

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router";
 import { AuthProvider } from "../contexts/auth-contexts";
-
+import { Toaster } from "react-hot-toast";
 function App() {
   return (
     <div>
       <AuthProvider>
+        <Toaster />
         <Outlet />
       </AuthProvider>
     </div>
