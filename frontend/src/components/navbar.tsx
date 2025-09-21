8cb6865f32f3e9e3a917444045ae97212f7211b1
lihasahil <sahilshrestha106@gmail.com>
2025-09-21 21:59:45 +0545

    feature: navbar layout

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,5 +1,46 @@
+import { Link } from "react-router";
+import { useAuth } from "../contexts/auth-contexts";
+import { LogOut, MessageSquare, Settings, User } from "lucide-react";
+import { Button } from "./ui/button";
+
 function Navbar() {
-  return <div>NAvbar</div>;
+  const { logout, user } = useAuth();
+  return (
+    <header className="bg-base-100 border-b border-base-300 fixed w-full top-0 z-40 backdrop-blur-lg bg-base-100/80">
+      <div className="flex items-center justify-between h-full">
+        <div className="flex items-center gap-8">
+          <Link
+            to="/home"
+            className="flex items-center gap-2.5 hover:opacity-80 transition-all"
+          >
+            <div className="w-9 h-9 rounded-lg bg-primary/10 flex items-center justify-center">
+              <MessageSquare className="w-5 h-5 text-primary" />
+            </div>
+            <h1 className="text-lg font-bold">GuffGaff</h1>
+          </Link>
+        </div>
+        <div className="flex items-center gap-2">
+          <Link to={"/settings"} className="btn btn-sm gap-2 transition colors">
+            <Settings className="size-4" />
+            <span className="hidden sm:inline">Settings</span>
+          </Link>
+          {user && (
+            <>
+              <Link to={"/profile"} className="btn btn-sm gap-2">
+                <User className="size-5" />
+                <span className="hidden sm:inline">Profile</span>
+              </Link>
+
+              <Button onClick={logout}>
+                <LogOut className="size-5" />
+                <span className="hidden sm:inline">LogOut</span>
+              </Button>
+            </>
+          )}
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Navbar;
