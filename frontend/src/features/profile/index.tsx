304e155deed1a66d9d1696071edc8123e0b00091
lihasahil <sahilshrestha106@gmail.com>
2025-09-27 20:48:20 +0545

    feature: profile page

--- frontend/src/features/profile/index.tsx
+++ frontend/src/features/profile/index.tsx
@@ -2,7 +2,13 @@ import { useAuth } from "../../contexts/auth-contexts";
 
 function ProfilePage() {
   const { user, updateProfile } = useAuth();
-  return <div>Profile</div>;
+  return (
+    <div className="flex items-center justify-center mt-10">
+      <div>{user?.fullName}</div>
+      <div>{user?.profilePic}</div>
+      <div>{user?.email}</div>
+    </div>
+  );
 }
 
 export default ProfilePage;
